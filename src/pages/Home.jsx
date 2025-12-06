0e57170ea2094be2769223b8c3525a2ddda35249
Shubham yadav <shubhamyadav1662000@gmail.com>
2025-12-06 19:11:01 +0530

    home page fron section designed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,18 +17,27 @@ const Home = () => {
                         muted
                     ></video>
 
-                    <div className='absolute top-20 left-0 sm:w-3/4 sm:left-20 w-full content-center md:left-30 sm:top-50 md:top-60 md:w-4/5  text-white bg-black/40  '>
-                        <div> <h1 className='text-3xl flex flex-row justify-center relative'> Discover <br /> Most
-                            Suitable  <br />Property  </h1>
+                    <div className='absolute top-20 left-0 sm:w-3/4 sm:left-20 w-full flex justify-center items-center md:left-30 sm:top-50 md:top-60 md:w-4/5  text-white bg-black/40  '>
+                        <div className='px-5 py-5'>
+                            <div className='text-2xl sm:gap-3 sm:text-5xl font-bold flex flex-col md:flex-row justify-center relative'>
+                                <h1>Discover</h1>
+                                <h1>Most Suitable</h1>
+                                <h1> Property  </h1>
 
+                            </div>
+                            <div className='hidden sm:flex flex-col justify-center items-center md:text-2xl'>
+                                <span>Search and find your ideal property without any hassle.</span><br />
+                                <span>Forget all the difficulties in finding a residence for you.</span>
+                            </div>
+                            <div className='flex justify-center'>
+                                <div className='bg-amber-50 sm:w-3/4 p-1 sm:p-2 rounded-2xl flex-row flex items-center sm:justify-between sm:gap-5'>
+                                    <HiLocationMarker className='text-blue-600 sm:text-2xl' />
+                                    <input type="text" placeholder='Location' className='sm:text-2xl text-black' />
+                                    <button className='bg-blue-600 rounded-2xl p-1 px-2 sm:text-2xl '>search</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className='flex flex-col justify-center items-center'>
-                            <span>Search and find your ideal property without any hassle.</span><br />
-                            <span>Forget all the difficulties in finding a residence for you.</span>
-                        </div>
-                        <div>
 
-                        </div>
                     </div>
 
 
